bb46460cce6a3961f1f8ee02039a89d9077b229d
Robert van der Klis <robert.vdklis@protonmail.com>
2026-01-12 20:07:45 +0100

    higher wd

---
+++ run_ourlinear_theirparams_warmup_wd1e3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=train_ourlinear_theirparams_warmup_wd1e3
+#SBATCH --output=./%x_%j.out
+#SBATCH --time=06:00:00
+#SBATCH --partition=normal
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=32
+#SBATCH --account=a132
+
+set -e
+
+conda activate HCNN
+
+python code/classification/train.py -c classification/config/L-ResNet18_ourlinear_theirparams_warmup_wd1e3.txt
